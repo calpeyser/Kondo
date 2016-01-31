2cd6f007c2104b06d0fecb0d83f9614bf51ba09a
calpeyser <calpeyser@gmail.com>
2016-01-31 13:08:10 -0500

    file upload endpoint in place

--- Kondo/server.py
+++ Kondo/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 app = Flask(__name__)
 
 # ---- Endpoints for serving static assets ----
@@ -9,8 +9,13 @@ def bootstrap_endpoint(path):
 @app.route('/upload_assets/<path:path>')
 def upload_assets_endpoint(path):
     return send_from_directory('static/upload_assets', path)
-
 # ---------------------------------------------
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+	x = request.files['file_data'].read()
+	print x
+	return x
 
 @app.route('/')
 def index():
