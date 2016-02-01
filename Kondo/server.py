76679a11e64db917f7d7b73ba0ede800c70e0163
calpeyser <calpeyser@gmail.com>
2016-01-31 22:10:48 -0500

    provisional RPC

--- Kondo/server.py
+++ Kondo/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, send_from_directory, request
+import json, requests
+
 app = Flask(__name__)
 
 # ---- Endpoints for serving static assets ----
@@ -11,15 +13,24 @@ def upload_assets_endpoint(path):
     return send_from_directory('static/upload_assets', path)
 # ---------------------------------------------
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-	x = request.files['file_data'].read()
-	print x
-	return x
+def html_from_errors(errors):
+	pass
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+	params = json.dumps({'chorale': request.files['file_data'].read()})
+	r = requests.post('http://192.168.99.101:8080/check', params)
+
+	print r.json()
+
+	return json.dumps({
+		'initialPreview': '<p>%s</p>' % r.json().errors[0],
+	})
 
 if __name__ == '__main__':
     app.run()
